94e22a66c84712904e0e913a4c2b4a2698a05e93
AleksandrKolinko <kolinko.sanya@mail.ru>
2024-05-27 10:48:26 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+my_string = input('Нажмите Enter ')
+text = 'Нажмите Enter'
+tring_length = len(text)
+(print('Длина строки ', tring_length, 'символов'))
+
+print('Нажмите Enter'.upper())
+print('Нажмите Enter'.lower())
+print('Нажмите Enter'.replace(' ', ''))
+print(text[0])
+print(text[-1])
