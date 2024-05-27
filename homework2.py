8676ab923b5756244e144601c98670dc4bc78b31
AleksandrKolinko <kolinko.sanya@mail.ru>
2024-05-27 10:44:09 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+completed_homework = 12
+a = 'completed_homework'
+time_spent = 1.5
+b = 'time_spent'
+name_course = 'Python'
+time_one_task = time_spent / completed_homework
+c = 'time_one_task'
+print(name_course, a, completed_homework, b, time_spent, c, time_one_task)
